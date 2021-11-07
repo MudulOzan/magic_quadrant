948cd6c3cfe5b0bbdf1ad287b66c3b5969a80599
Ozan Mudul <mudulozan@gmail.com>
2021-11-07 15:10:31 +0300

    - added missing types - any types are assigned to their appropriate types

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
 import './App.css';
 
 type dot = {
-	id: number,
-	x: number,
-	y: number,
-	label: string
+	id?: number,
+	x?: number,
+	y?: number,
+	label?: string
+	[key: string]: any
+}
+
+type draggable = {
+	dragging?: boolean,
+	X?: number,
+	Y?: number,
+	oldX?: number,
+	oldY?: number,
+	id?: number
+}
+
+type quadrantStorage = {
+	dots: dot[] | undefined,
+	draggable: draggable | undefined,
+	lastId: number | undefined
 }
 
 function App() {
 	const containerRef = useRef<any>();
 
-	const [draggable, setDraggable] = useState<any>({});
-	const [dots, setDots] = useState<any[]>([
+	const [draggable, setDraggable] = useState<draggable>();
+	const [dots, setDots] = useState<dot[]>([
 		{ id: 1, x: Math.floor((Math.random() * 70) + 10), y: Math.floor((Math.random() * 60) + 30), label: "1" },
 		{ id: 2, x: Math.floor((Math.random() * 70) + 10), y: Math.floor((Math.random() * 60) + 30), label: "2" },
 		{ id: 3, x: Math.floor((Math.random() * 70) + 10), y: Math.floor((Math.random() * 60) + 30), label: "3" }
@@ -20,29 +36,31 @@ function App() {
 	const [lastId, setLastId] = useState<number>(4);
 
 	useEffect(() => {
-		let tempData = localStorage.getItem("data");
+		let tempData: string | null = localStorage.getItem("data");
 		if (tempData) {
-			let parsed = JSON.parse(tempData)
-			setDots(parsed.dots)
-			setDraggable({ ...parsed.draggable })
-			setLastId(parsed.lastId)
+			let parsed: quadrantStorage = JSON.parse(tempData)
+			parsed.dots && setDots(parsed.dots)
+			parsed.draggable && setDraggable({ ...parsed.draggable })
+			parsed.lastId && setLastId(parsed.lastId)
 		}
 
 	}, [])
 
-	const move = (e: any) => {
-		let tempDots = [...dots];
-		let index = tempDots.findIndex((d: dot) => d.id === draggable.id);
-		let item = tempDots[index]
-		item.x = (Number(draggable.oldX) + (e.pageX - draggable.X) / 4);
-		item.y = (Number(draggable.oldY) - (e.pageY - draggable.Y) / 4);
-		tempDots[index] = item;
-		setDots(tempDots);
+	const move = (e: React.MouseEvent<any>) => {
+		if (draggable && draggable.X && draggable.Y) {
+			let tempDots: dot[] = [...dots];
+			let index: number = tempDots.findIndex((d: dot) => d.id === draggable.id);
+			let item: dot = tempDots[index]
+			item.x = (Number(draggable.oldX) + (e.pageX - draggable.X) / 4);
+			item.y = (Number(draggable.oldY) - (e.pageY - draggable.Y) / 4);
+			tempDots[index] = item;
+			setDots(tempDots);
+		}
 	}
 
 	useEffect(() => {
-		let node = containerRef.current;
-		if (draggable.dragging) {
+		let node: any = containerRef.current;
+		if (draggable?.dragging) {
 			node.addEventListener('mousemove', move)
 			node.addEventListener('mouseup', onMouseUp)
 		}
@@ -56,7 +74,7 @@ function App() {
 	})
 
 	useEffect(() => {
-		const data = {
+		const data: quadrantStorage = {
 			draggable,
 			dots,
 			lastId
@@ -71,24 +89,23 @@ function App() {
 			y: 50,
 			label: "New Item"
 		}
-		setDots((prevState: any) => ([
+		setDots((prevState: dot[]) => ([
 			...prevState,
 			newDot
 		]));
 		setLastId(lastId + 1);
 	}
 
-	const onMouseUp = (e: any) => {
-		setDraggable((prevState: any) => ({
-			...prevState,
+	const onMouseUp = (e: React.MouseEvent<any>) => {
+		setDraggable({
 			dragging: false,
-			id: null
-		}))
+			id: -1
+		})
 		e.stopPropagation()
 		e.preventDefault()
 	}
 
-	const handleMouseDown = (e: any, item: dot) => {
+	const handleMouseDown = (e: React.MouseEvent<any>, item: dot) => {
 		if (e.button !== 0) return
 
 		setDraggable({
@@ -103,18 +120,18 @@ function App() {
 		e.preventDefault()
 	}
 
-	const handleDelete = (item: any): void => {
-		let tempDots = [...dots];
-		let index = tempDots.findIndex((d: dot) => d.id === item.id);
+	const handleDelete = (item: dot): void => {
+		let tempDots: dot[] = [...dots];
+		let index: number = tempDots.findIndex((d: dot) => d.id === item.id);
 		tempDots.splice(index, 1);
 		setDots(tempDots);
 
 	}
 
-	const handleChange = (e: React.ChangeEvent<any>, item: any): void => {
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>, item: dot): void => {
 		const { target } = e;
-		let tempDots = [...dots];
-		let index = tempDots.findIndex((d: dot) => d.id === item.id);
+		let tempDots: dot[] = [...dots];
+		let index: number = tempDots.findIndex((d: dot) => d.id === item.id);
 		item[target.name] = target.value;
 		tempDots[index] = item;
 		setDots(tempDots);
@@ -136,8 +153,8 @@ function App() {
 							{dots.map((item: dot) => {
 								return (
 									<React.Fragment key={item.id}>
-										<g className="node" transform={`translate(${item.x * 4},${400 - item.y * 4})`} onMouseDown={(e: React.ChangeEvent<any>) => handleMouseDown(e, item)}>
-											<circle r="27" className={`dot-circle ${draggable.id === item.id ? 'dot-circle-active' : ''}`}></circle>
+										<g className="node" transform={`translate(${item.x ? item.x * 4 : 50},${item.y ? 400 - item.y * 4 : 50})`} onMouseDown={(e: React.MouseEvent<any>) => handleMouseDown(e, item)}>
+											<circle r="27" className={`dot-circle ${draggable?.id === item.id ? 'dot-circle-active' : ''}`}></circle>
 											<circle r="7.5" className="dot"></circle>
 											<text dy="20" dx="5" className="label-point">{item.label}</text>
 										</g>
@@ -162,9 +179,9 @@ function App() {
 								return (
 									<React.Fragment key={item.id}>
 										<tr>
-											<td><input value={item.label} name="label" onChange={(e: React.ChangeEvent<any>) => handleChange(e, item)} /></td>
-											<td><input value={item.x} name="x" onChange={(e: React.ChangeEvent<any>) => handleChange(e, item)} /></td>
-											<td><input value={item.y} name="y" onChange={(e: React.ChangeEvent<any>) => handleChange(e, item)} /></td>
+											<td><input value={item.label} name="label" onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e, item)} /></td>
+											<td><input value={item.x} name="x" onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e, item)} /></td>
+											<td><input value={item.y} name="y" onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e, item)} /></td>
 											<td><button className="btn btn-sm btn-delete" onClick={() => handleDelete(item)}>Delete</button></td>
 										</tr>
 									</React.Fragment>
